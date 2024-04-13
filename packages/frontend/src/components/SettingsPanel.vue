a35e8254c45a12b0d5439a0fb9d76e4640f6c902
“Saphron” <109723086+denerose@users.noreply.github.com>
2024-04-13 14:21:03 +1000

    Add LunchDays to Prisma

--- packages/frontend/src/components/SettingsPanel.vue
+++ packages/frontend/src/components/SettingsPanel.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import { getSettingsFromServer, updateSettingsOnServer } from '../data/helpers';
 import { SettingsData } from '../data/types';
+import LunchDaysTable from './LunchDaysTable.vue';
 
 const serverSettings = await getSettingsFromServer()
 
@@ -21,22 +22,28 @@ const submitForm = async () => {
 
 <template>
     <div>
-        <h3>Edit Settings</h3>
-        <form @submit.prevent="submitForm">
-            <div>
-                <label for="lunch-rule">Check for lunches:</label>
-                <input v-model="settingsData.lunchRule" type="checkbox" id="lunch-rule" />
-            </div>
-            <div>
-                <label for="carb-rule">Avoid double carbs:</label>
-                <input v-model="settingsData.carbRule" type="checkbox" id="carb-rule" />
-            </div>
-            <div>
-                <label for="acid-rule">Avoid double acid:</label>
-                <input v-model="settingsData.acidRule" type="checkbox" id="acid-rule" />
-            </div>
-            <button type="submit">Update Settings</button>
-        </form>
+        <details>
+            <summary open>Suggestion Rules</summary>
+            <form @submit.prevent="submitForm">
+                <div>
+                    <label for="lunch-rule">Check for lunches:</label>
+                    <input v-model="settingsData.lunchRule" type="checkbox" id="lunch-rule" />
+                </div>
+                <div>
+                    <label for="carb-rule">Avoid double carbs:</label>
+                    <input v-model="settingsData.carbRule" type="checkbox" id="carb-rule" />
+                </div>
+                <div>
+                    <label for="acid-rule">Avoid double acid:</label>
+                    <input v-model="settingsData.acidRule" type="checkbox" id="acid-rule" />
+                </div>
+                <button type="submit">Update Settings</button>
+            </form>
+        </details>
+        <details>
+            <summary>Lunch Days</summary>
+            <LunchDaysTable />
+        </details>
     </div>
 </template>
 
