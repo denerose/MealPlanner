faf965b0ad2b647655d12073f2e8dcd5393d370a
denerose <109723086+denerose@users.noreply.github.com>
2024-05-10 11:46:41 +1000

    Make ingredient list pretty

--- packages/frontend/src/components/IngredientList.vue
+++ packages/frontend/src/components/IngredientList.vue
@@ -8,19 +8,21 @@ if (store.allIngredients.length == 0) { store.fetchIngredients() }
 
 </script>
 <template>
-    <div class="card">
-        <ul>
-            <li v-for="ing in store.allIngredients">
-                <div class="item-div"><span class="item">{{ ing.ingredientName }}</span>
-                    <XMarkIcon @click="store.pushDeleteIngredient(ing)" class="del">
-                    </XMarkIcon>
+    <div class="card-body">
+        <ul class="list-group list-group-horizontal-md flex-wrap">
+            <li v-for="ing in store.allIngredients" class="list-group-item col-12 col-sm-3 col-lg-2 border-0">
+                <div class="card p-2">
+                    <div class="d-flex"><span class="item">{{ ing.ingredientName }}</span>
+                        <XMarkIcon @click="store.pushDeleteIngredient(ing)" class="del ms-auto">
+                        </XMarkIcon>
+                    </div>
                 </div>
             </li>
         </ul>
     </div>
 </template>
 <style scoped>
-.item-div {
+/* .item-div {
     display: flex;
     min-width: 200px;
     max-width: 40%;
@@ -29,7 +31,7 @@ if (store.allIngredients.length == 0) { store.fetchIngredients() }
 .item {
     min-width: 100px;
     flex-grow: 2;
-}
+} */
 
 .edit {
     color: rgb(95, 52, 213);
