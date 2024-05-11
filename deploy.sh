de9c6b0e95b21f6febb9b526b4cc13ee637d60c3
Kim Dowling <kim@phcyso.com>
2024-05-11 13:41:56 +1000

    update deploy script more to handle bare git folders

--- deploy.sh
+++ deploy.sh
@@ -26,26 +26,26 @@ run_ssh_command() {
 set_or_exit "SSH_DEPLOY_USERNAME"
 set_or_exit "SSH_DEPLOY_HOSTNAME"
 
-TARGER_FOLDER="app"
+APP_TARGET_FOLDER="app" # Where the checked out code will live
+GIT_TARGET_FOLDER="app.git" # Where the git worktree will live
 
 GIT_DEPLOY_BRANCH="kd/finish-docker-compose-setup-for-deployment"
 
 # set up the local remote
-git remote add llamaserver ssh://${SSH_DEPLOY_USERNAME}@${SSH_DEPLOY_HOSTNAME}:/home/mealplanner/app
+git remote add llamaserver ssh://${SSH_DEPLOY_USERNAME}@${SSH_DEPLOY_HOSTNAME}:/home/mealplanner/${GIT_TARGET_FOLDER}
 
 # setup the remote folder for git if its not done already
 # shellcheck disable=SC2029
-run_ssh_command "git init ${TARGER_FOLDER}"
+run_ssh_command "git init --bare ${GIT_TARGET_FOLDER}"
 
 # push the code to the server
 git push --force -v llamaserver ${GIT_DEPLOY_BRANCH}
 
-run_ssh_command "cd ${TARGER_FOLDER} && git checkout ${GIT_DEPLOY_BRANCH}"
+run_ssh_command "git --work-tree ${APP_TARGET_FOLDER} --git-dir=${GIT_TARGET_FOLDER}/ checkout -f ${GIT_DEPLOY_BRANCH}"
 
 # tear down the old docker containers
 run_ssh_command "cd ${TARGER_FOLDER} && docker compose down -v"
 
 run_ssh_command "cd ${TARGER_FOLDER} && docker compose up -d --build"
-
 
 
