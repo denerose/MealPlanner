e8f0018bf5aa551396fc1f7c10cce526f6fcc9ea
Kim Dowling <kim@phcyso.com>
2024-05-11 14:18:45 +1000

    fixup vite port

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3333',
+        target: 'http://localhost:3200',
         changeOrigin: true,
         secure: false,
         ws: true,
